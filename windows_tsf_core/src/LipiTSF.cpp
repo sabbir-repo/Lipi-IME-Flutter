9480a7bfa838b4dbb5493d4405b579aef92cf6ca
Savvy Staks <savvystaks@gmail.com>
2026-07-23 20:15:02 +0600

    Auto-commit: Fixed clear command leaving the composition string active in the editor

--- windows_tsf_core/src/LipiTSF.cpp
+++ windows_tsf_core/src/LipiTSF.cpp
@@ -416,14 +416,26 @@ HRESULT CLipiTSF::_DoEditSession(TfEditCookie ec, ITfContext *pic, WPARAM wParam
     }
 
     if (_currentWord == L"clear") {
-            _ipc.SendMessage(L"CLEAR_CACHE");
-            _currentWord.clear();
-            _lastFetchedWord.clear();
-            _suggestions.clear();
-            _selectedIndex = 0;
-            _ipc.SendMessage(L"HIDE");
-            return S_OK;
+        if (_pComposition) {
+            ITfRange *pRange = NULL;
+            if (SUCCEEDED(_pComposition->GetRange(&pRange))) {
+                pRange->SetText(ec, 0, L"", 0);
+                pRange->Release();
+            }
+            ITfComposition *pComp = _pComposition;
+            _pComposition = NULL;
+            pComp->EndComposition(ec);
+            pComp->Release();
         }
+
+        _ipc.SendMessage(L"CLEAR_CACHE");
+        _currentWord.clear();
+        _lastFetchedWord.clear();
+        _suggestions.clear();
+        _selectedIndex = 0;
+        _ipc.SendMessage(L"HIDE");
+        return S_OK;
+    }
 
     bool needsFetch = false;
     if (wParam == 0x10000) {
